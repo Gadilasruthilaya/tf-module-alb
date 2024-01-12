fa876fe2f5974529d3694593841bba59e20aab76
sruthilaya <sruthilayagadila@gmail.com>
2024-01-12 22:45:20 +0530

    app

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_lb" "main" {
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
   port              = 443
-  protocol          = "HTTP"
+  protocol          = "HTTPS"
 
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = "arn:aws:acm:us-east-1:190338077320:certificate/bce7eee6-45f4-45ae-874c-f5c41019f556"
